21410e4890ab09275e1c79a81682860c4bd58cd3
Stanco27 <Stancodiaz@gmail.com>
2024-08-18 18:56:20 -0700

    website build

--- src/route.tsx
+++ src/route.tsx
@@ -1,23 +1,25 @@
-import { Route, Routes } from "react-router-dom"
-import MiniPage from "./Mini-Game/mini-page"
-import TerminalPage from "./Terminal/terminal-page"
-import NormalPage from "./Normal/normal-page"
-import MainPage from "./Main-Page/main-page"
-import ContactPage from "./Contact/contact-page"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MiniPage from "./Mini-Game/mini-page";
+import TerminalPage from "./Terminal/terminal-page";
+import NormalPage from "./Normal/normal-page";
+import MainPage from "./Main-Page/main-page";
+import ContactPage from "./Contact/contact-page";
 
 const AppRoute = () => {
   return (
     <>
-    <Routes>
-      <Route path={"/"} element={<MainPage />} />
-      <Route path={"/Home"} element={<MainPage />} />
-      <Route path="/Mini" element={<MiniPage />}/>
-      <Route path="/Terminal" element={<TerminalPage />}/>
-      <Route path="/Normal" element={<NormalPage />}/>
-      <Route path="/Contact" element={<ContactPage />} />
-    </Routes>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path={"/"} element={<MainPage />} />
+          <Route path={"/Home"} element={<MainPage />} />
+          <Route path="/Mini" element={<MiniPage />} />
+          <Route path="/Terminal" element={<TerminalPage />} />
+          <Route path="/Normal" element={<NormalPage />} />
+          <Route path="/Contact" element={<ContactPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
 export default AppRoute;
