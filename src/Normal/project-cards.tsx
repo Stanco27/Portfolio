8a4493ba19721772020eafb48098adfc22187865
Stanco27 <Stancodiaz@gmail.com>
2024-08-19 18:36:37 -0700

    debug

--- src/Normal/project-cards.tsx
+++ src/Normal/project-cards.tsx
@@ -4,20 +4,20 @@ import '../Normal/normal-page.css'
 
 interface Projects {
   name: string;
-  image: string;
   link: string;
 }
 
 interface ProjectProps {
+  image: string;
   project: Projects;
 }
 
-const ProjectCards: React.FC<ProjectProps> = ({ project }) => {
+const ProjectCards: React.FC<ProjectProps> = ({ project, image }) => {
   return (
     <Link to={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
       <Card className="project-card">
         <Card.Title className="py-2 project-title">{project.name}</Card.Title>
-        <Card.Img className="project-img" src={project.image} />
+        <Card.Img className="project-img" src={image} />
       </Card>
     </Link>
   );
