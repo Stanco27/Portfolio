ac3e780b2a9d7f4378e9a7dbae42b03215872879
Stanco27 <Stancodiaz@gmail.com>
2025-08-26 13:19:06 -0700

    Enhance footer with project and contact links, refine project details
    Footer Enhancement:
    - Added clickable links to individual project pages within the footer.
    - Incorporated a direct link to the contact form for easy reachability.
    - Ensured all new links open in a new tab for improved user experience.
    
    Project Information Refinement:
    - Applied minor textual and presentation adjustments to project descriptions for clarity and impact.
    - Reviewed and corrected any inconsistencies in project details.

--- src/footer.tsx
+++ src/footer.tsx
@@ -3,17 +3,24 @@ import '../src/footer.css';
 
 const Footer = () => {
   return (
+    <>
     <Row className='footer g-0'>
         <Col className='footer-sections'>
         <h3>Projects</h3>
-        <h6 onClick={() => console.log()}>Market App</h6>
-        <h6 onClick={() => console.log()}>RigAssembly</h6>
+        <h6 className="footer-text" onClick={() => window.open("https://my-meeting-notetaker.vercel.app")}>My Meeting Notaker</h6>
+        <h6 className="footer-text" onClick={() => window.open("https://github.com/TomasHerreraS/market-app")}>Market App</h6>
+        <h6 className="footer-text" onClick={() => window.open("https://stanco27.github.io/RigAssembly/")}>RigAssembly</h6>
         </Col>
         <Col className='footer-sections'>
         <h3>Contact</h3>
-        <h6>stancodiaz@gmail.com</h6>
+        <h6 className='footer-text'>stancodiaz@gmail.com</h6>
+        <h6 className='footer-text' onClick={() => window.open("https://github.com/Stanco27")}>Github</h6>
+        <h6 className='footer-text' onClick={() => window.open("https://www.linkedin.com/in/stanco-diaz-b418a117b/")}>LinkedIn</h6>
         </Col>
     </Row>
+    <p className='copyright'>&copy; 2024 Stanco Diaz. All Rights Reserved.</p>
+    </>
+    
   )
 }
 
